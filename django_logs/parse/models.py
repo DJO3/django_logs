15b399d3a1c2261bdf5f72e9f02af1e37bfef81c
Dave O'Connor <dev@pytools.org>
2015-07-08 20:08:16 -0400

    added Entry model

--- django_logs/parse/models.py
+++ django_logs/parse/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Entry(models.model):
+    real_ip = models.CharField(max_length=200),
+    time = models.DateTimeField(auto_now=False),
+    request = models.CharField(max_length=6),
+    page = models.CharField(max_length=200),
+    status = models.CharField(max_length=3),
+    bytes_sent = models.CharField(max_length=10),
+    referer = models.CharField(max_length=200),
+    user_agent = models.CharField(max_length=200),
+    mobile = models.BooleanField()
