6d153bd3cfd0b00dcd30e2117a8ddf9c75184653
Dave O'Connor <dev@pytools.org>
2015-07-08 20:59:47 -0400

    added Entry test, updated test db

--- django_logs/django_logs/settings.py
+++ django_logs/django_logs/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import sys
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -37,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'parse',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -81,6 +83,8 @@ DATABASES = {
     }
 }
 
+if 'test' in sys.argv:
+    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
--- django_logs/parse/tests.py
+++ django_logs/parse/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from .parse_access_logs import Parse
+from .models import Entry
 
-# Create your tests here.
+
+class ParseTestCase(TestCase):
+    def test_entry_setup(self):
+        path = "/Users/dave/Downloads/log"
+        logs = Parse(path)
+        entries = logs.parse()
+
+        for entry in entries:
+            Entry.objects.get_or_create(
+                real_ip = entry["real_ip"],
+                time = entry["time"],
+                request = entry["request"],
+                page = entry["page"],
+                status = entry["status"],
+                bytes_sent = entry["bytes_sent"],
+                referer = entry["referer"],
+                user_agent = entry["user_agent"],
+                mobile = entry["mobile"]
+            )
