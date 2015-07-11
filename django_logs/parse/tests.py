74fa67efc3a24d65199329352c7eda304978f0e2
Dave O'Connor <dev@pytools.org>
2015-07-10 21:19:27 -0400

    added setup to test
    tests for parse  script and entry model

--- django_logs/parse/tests.py
+++ django_logs/parse/tests.py
@@ -1,10 +1,28 @@
 from django.test import TestCase
+from datetime import datetime
 from .parse_access_logs import Parse
 from .models import Entry
 
 
 class ParseTestCase(TestCase):
-    def test_entry_setup(self):
+    def setUp(self):
+        Entry.objects.create(
+            real_ip = "192.168.1.12",
+            time = datetime.strptime("23/Jun/2015:11:10:57 +0000", "%d/%b/%Y:%H:%M:%S %z"),
+            request = "GET",
+            page = "/entry/how-create-configure-free-ssl-certificate-using-django-and-pythonanywhere",
+            status = "200",
+            bytes_sent = "5",
+            referer = "http://www.reddit.com/r/Python/",
+            user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.18 Safari/537.36",
+            mobile = False
+        )
+
+    def test_entry(self):
+        entry = Entry.objects.get(real_ip="192.168.1.12")
+        self.assertEqual(entry.real_ip, "192.168.1.12")
+
+    def test_entry_parse_script(self):
         path = "/Users/dave/Downloads/log"
         logs = Parse(path)
         entries = logs.parse()
